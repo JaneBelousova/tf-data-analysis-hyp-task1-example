cc5648a574479397c7031f1342cd00499cedddf4
JaneBelousova <42033480+JaneBelousova@users.noreply.github.com>
2023-04-13 13:48:18 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import norm
 
 chat_id = 1124136722 # Ваш chat ID, не меняйте название переменной
 
@@ -12,4 +12,7 @@ def solution(x_success: int,
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alfa=0.07
-    return (y_success/y_cnt - x_success/x_cnt) > alfa*(y_success/y_cnt) # Ваш ответ, True или False
+    threshold = x_success/x_cnt
+    conversion = y_success/y_cnt # наше ограничение слева
+    p = norm.cdf(np.sqrt(y_cnt) * (conversion - threshold) / np.sqrt(conversion * (1 - conversion)))
+    return p >= (1 - alfa) # Ваш ответ, True или False
